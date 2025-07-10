f9422d700f9e6594d9f598faad050c23025e8ad3
Nate Lee <mengyong.lee@thoughtworks.com>
2025-07-10 09:37:47 +0800

    fix flake8

--- test_app.py
+++ test_app.py
@@ -1,9 +1,11 @@
 import unittest
 from app import get_greeting
 
+
 class TestApp(unittest.TestCase):
     def test_get_greeting(self):
         self.assertEqual(get_greeting(), "Hello, world! This is a Streamlit app.")
 
+
 if __name__ == "__main__":
-    unittest.main() 
+    unittest.main()
